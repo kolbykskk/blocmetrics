debf25cd7c77624484da9d21ac1211c3a82b35f1
Kolby <kolbykalafut@gmail.com>
2018-03-28 18:14:39 -0400

    Checkpoint 5 api: completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   get 'welcome/index'
 
   root 'welcome#index'
